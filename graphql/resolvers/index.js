8c6244bc477e24f09269db78ca7b62dee44d6719
Chaminda Indunil <chaminda.indunil@hotmail.com>
2022-09-21 08:01:18 +0530

    add schemas and resolvers to seperate module

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,94 @@
+const bcrypt = require('bcryptjs');
+
+const Event = require('../../models/event');
+const User = require('../../models/user');
+const Booking = require('../../models/booking');
+
+const events = async eventIds => {
+    try {
+        const events = await Event.find({ _id: { $in: eventIds } });
+        events.map(event => {
+                return { ...event._doc, date: new Date(event._doc.date).toISOString(), creator: user.bind(this, event.creator) };
+        });
+        return events;
+    } catch (err) {
+        throw err;
+    }
+};
+
+const user = async userId => {
+    try {
+        const user = await User.findById(userId);
+        return { ...user._doc, createdEvents: events.bind(this, user._doc.createdEvents) };
+    } catch (err) {
+        throw err;
+    }
+    
+};
+
+module.exports = {
+    events: async () => {
+        try {
+            const events = await Event.find();
+            return events.map(event => {
+                return { ...event._doc, date: new Date(event._doc.date).toISOString(), creator: user.bind(this, event._doc.creator) };
+            });
+        } catch (err) {
+            throw err;
+        }
+    },
+    bookings: async () => {
+        try {
+            const bookings = await Booking.find();
+            return bookings.map(booking => {
+                return { ...booking._doc, createdAt: new Date(booking._doc.createdAt).toISOString(), updatedAt: new Date(booking._doc.updatedAt).toISOString() };
+            });
+        } catch (err) {
+            throw err;
+        }
+    },
+    createEvent: async (args) => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: args.eventInput.price,
+            date: new Date(args.eventInput.date),
+            creator: '632a6d335ae6d227f7b1dd59'
+        });
+        let createdEvent;
+        try {
+            const result = await event.save();
+            createdEvent = { ...result._doc, date: new Date(event._doc.date).toISOString(), creator: user.bind(this, result._doc.creator) };
+            const creator = await User.findById('632a6d335ae6d227f7b1dd59');
+        
+            if (!creator) {
+                throw new Error('Not found user.');
+            }
+            creator.createdEvents.push(event);
+            await creator.save();
+            return createdEvent;
+        } catch (err) {
+            throw err;
+        }
+    },
+    createUser: async args => {
+        try {
+            const existingUser = await User.findOne({ email: args.userInput.email });
+            if (existingUser) {
+                throw new Error('User exists already.');
+            }
+            const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+            const user = new User({
+                email: args.userInput.email,
+                password: hashedPassword
+            });
+            const result = await user.save();
+        
+            return { ...result._doc, password: null, _id: result.id };
+        } catch (err) {
+            throw err;
+        }
+        
+
+    }
+}
