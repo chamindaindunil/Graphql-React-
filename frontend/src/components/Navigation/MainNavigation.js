efc0d639edd24e84a6b2a6b40b4d2293a6d88842
Chaminda Indunil <chaminda.indunil@hotmail.com>
2022-09-23 08:17:39 +0530

    intiate react frontend

---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const mainNavigation = props => (
+    <header>
+        <div className="main-navigation__logo">
+            <h1>EasyEvent</h1>
+        </div>
+        <nav className="main-navigation__item">
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+);
+
+export default mainNavigation;
