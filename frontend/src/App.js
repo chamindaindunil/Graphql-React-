826e6e8fe4bfb5674e9dcd248b56b464303e2070
Chaminda Indunil <chaminda.indunil@hotmail.com>
2022-09-26 09:00:04 +0530

    add modal and logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,6 @@ class App extends Component {
             <MainNavigation />
             <main className="main-content">
               <Switch>
-                {!this.state.token && <Redirect from="/" to="/auth" exact />}
                 {this.state.token && <Redirect from="/" to="/events" exact />}
                 {this.state.token && <Redirect from="/auth" to="/events" exact />}
                 {!this.state.token && (
@@ -41,6 +40,7 @@ class App extends Component {
                 {this.state.token && (
                   <Route path="/bookings" component={BookingsPage} />
                 )}
+                {!this.state.token && <Redirect to="/auth" exact />}
               </Switch>
             </main>
           </AuthContext.Provider>
